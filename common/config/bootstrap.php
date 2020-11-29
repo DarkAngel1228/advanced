676537a3a5d8735b9f289b2f2f54ab3b6de45ba0
unknown <363626256@qq.com>
2020-11-29 10:07:15 +0800

    setAlias-services

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
+
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+/**
+ * @zhangjinyu
+ * 增加services层
+ */
+Yii::setAlias('@services', dirname(dirname(__DIR__)) . '/services');
